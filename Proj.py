6e7c31c5a253a74d89483eeb89d96d5f8c4555e8
DangerGrandma <alexjolic@hotmail.com>
2021-11-08 19:12:02 -0500

    Find External Nodes

--- Proj.py
+++ Proj.py
@@ -1,6 +1,20 @@
 from RPQ import loadgraph, runquery
 
 
+def get_incoming_nodes(local_name, local_file, external_name, external_file):
+    gin = loadgraph(local_file)
+    #gout = loadgraph(external_file)
+    node_in = local_name.lower()
+    node_ext = external_name.lower()
+    nodes = []
+    for val in external_file:
+        if val.split(":")[0] != node_in:
+            continue
+        else:
+            if val.split(":")[0].lower() == node_in:
+                nodes.append(node_ext + " " + val)
+    return nodes
+
 def get_outgoing_nodes(domain_name, filename):
     g = loadgraph(filename)
     nodes_out = set()
@@ -16,4 +30,6 @@ def get_outgoing_nodes(domain_name, filename):
 
 
 result = get_outgoing_nodes("green", "Graph Green.txt")
-print(result)
+red = get_outgoing_nodes("red", "Graph Red.txt")
+test = get_incoming_nodes("red", "Graph Red.txt", "green", result)
+print(test)
